65931b42124487e18f7dd47bdfd305766b7835cd
Marlon Bernardes <marlonjb.cb@gmail.com>
2015-12-20 17:22:23 -0200

    Added test for .ref(refName)

--- src/__tests__/ReactWrapper-spec.js
+++ src/__tests__/ReactWrapper-spec.js
@@ -1158,4 +1158,23 @@ describeWithDOM('mount', () => {
     });
   });
 
+  describe('.ref(refName)', () => {
+    it('gets a wrapper of the node matching the provided refName', () => {
+
+      class Foo extends React.Component {
+        render() {
+          return (
+            <div>
+              <span ref="firstRef" amount={2}>First</span>
+              <span ref="secondRef" amount={4}>Second</span>
+              <span ref="thirdRef" amount={8}>Third</span>
+            </div>
+          );
+        }
+      }
+      const wrapper = mount(<Foo />);
+      expect(wrapper.ref('secondRef').prop('amount')).to.equal(4);
+      expect(wrapper.ref('secondRef').text()).to.equal('Second');
+    });
+  });
 });
