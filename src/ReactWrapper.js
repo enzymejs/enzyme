597edb4e15b4f5a4db1b0da9975b296c5de459e7
Nuno Campos <nuno.campos@me.com>
2016-05-12 09:13:56 +0100

    Fix mount::simulate() signature (#381)
    * fix mount::simulate() signature
    
    * added default value for mock
    
    * updated docs to reflect arg name change

--- src/ReactWrapper.js
+++ src/ReactWrapper.js
@@ -468,10 +468,10 @@ export default class ReactWrapper {
    * testing events should be met with some skepticism.
    *
    * @param {String} event
-   * @param {Array} args
+   * @param {Object} mock (optional)
    * @returns {ReactWrapper}
    */
-  simulate(event, ...args) {
+  simulate(event, mock = {}) {
     this.single(n => {
       const mappedEvent = mapNativeEventNames(event);
       const eventFn = Simulate[mappedEvent];
@@ -479,7 +479,7 @@ export default class ReactWrapper {
         throw new TypeError(`ReactWrapper::simulate() event '${event}' does not exist`);
       }
 
-      eventFn(findDOMNode(n), ...args);
+      eventFn(findDOMNode(n), mock);
     });
     return this;
   }
