f40dae8b975b6e2acf685af87b65a5fed3e89251
Jacob Walton <jacob.walton@which.co.uk>
2016-09-01 07:45:06 +0200

    Remove redundant `exposedProperties` array (#571)

--- withDom.js
+++ withDom.js
@@ -2,13 +2,10 @@ if (!global.document) {
   try {
     const jsdom = require('jsdom').jsdom; // could throw
 
-    const exposedProperties = ['window', 'navigator', 'document'];
-
     global.document = jsdom('');
     global.window = document.defaultView;
     Object.keys(document.defaultView).forEach((property) => {
       if (typeof global[property] === 'undefined') {
-        exposedProperties.push(property);
         global[property] = document.defaultView[property];
       }
     });
