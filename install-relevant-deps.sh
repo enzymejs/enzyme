baf702de88b8de936318ac950d909851c34b6cda
indexzero <charlie.robbins@gmail.com>
2017-08-21 20:28:11 -0400

    [dist bin reactor] Update to `jsdom@^7.2.2` since we now only support node@4 or greater.Rename to install-relevant-deps.sh. Update .travis.yml build matrix.

--- install-relevant-deps.sh
+++ install-relevant-deps.sh
@@ -23,3 +23,25 @@ fi
 if [ "$REACT" = "16" ]; then
     npm run react:16
 fi
+
+JSDOM=${JSDOM:-7}
+
+if [ "$JSDOM" = "7" ]; then
+    npm run env -- jsdom:7
+fi
+
+if [ "$JSDOM" = "8" ]; then
+    npm run env -- jsdom:8
+fi
+
+if [ "$JSDOM" = "9" ]; then
+    npm run env -- jsdom:9
+fi
+
+if [ "$JSDOM" = "10" ]; then
+    npm run env -- jsdom:10
+fi
+
+if [ "$JSDOM" = "11" ]; then
+    npm run env -- jsdom:11
+fi
