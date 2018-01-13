f4f87c65027ba3616e1bdc7637fd4ecd6deb1c67
Jordan Harband <ljharb@gmail.com>
2018-01-13 01:35:16 -0800

    [Fix] improve "bad adapter" error message
    Fixes #1437

---
+++ packages/enzyme-test-suite/test/_helpers/adapter.js
@@ -0,0 +1,29 @@
+/* eslint global-require: 0, import/no-extraneous-dependencies: 0, import/no-unresolved: 0 */
+/**
+ * This file is needed only because we run our unit tests on multiple
+ * versions of React at a time. This file basically figures out which
+ * version of React is loaded, and exports the correct adapter for configuring.
+ */
+const {
+  REACT013,
+  REACT014,
+  REACT15,
+  REACT155,
+  REACT16,
+} = require('./version');
+
+let Adapter = null;
+
+if (REACT013) {
+  Adapter = require('enzyme-adapter-react-13');
+} else if (REACT014) {
+  Adapter = require('enzyme-adapter-react-14');
+} else if (REACT155) {
+  Adapter = require('enzyme-adapter-react-15');
+} else if (REACT15) {
+  Adapter = require('enzyme-adapter-react-15.4');
+} else if (REACT16) {
+  Adapter = require('enzyme-adapter-react-16');
+}
+
+module.exports = Adapter;
--- packages/enzyme-test-suite/test/_helpers/setupAdapters.js
+++ packages/enzyme-test-suite/test/_helpers/setupAdapters.js
@@ -1,31 +1,4 @@
-/* eslint global-require: 0, import/no-extraneous-dependencies: 0, import/no-unresolved: 0 */
-/**
- * This file is needed only because we run our unit tests on multiple
- * versions of React at a time. This file basically figures out which
- * version of React is loaded, and configures enzyme to use the right
- * corresponding adapter.
- */
-const {
-  REACT013,
-  REACT014,
-  REACT15,
-  REACT155,
-  REACT16,
-} = require('./version');
 const Enzyme = require('enzyme');
-
-let Adapter = null;
-
-if (REACT013) {
-  Adapter = require('enzyme-adapter-react-13');
-} else if (REACT014) {
-  Adapter = require('enzyme-adapter-react-14');
-} else if (REACT155) {
-  Adapter = require('enzyme-adapter-react-15');
-} else if (REACT15) {
-  Adapter = require('enzyme-adapter-react-15.4');
-} else if (REACT16) {
-  Adapter = require('enzyme-adapter-react-16');
-}
+const Adapter = require('./adapter');
 
 Enzyme.configure({ adapter: new Adapter() });
