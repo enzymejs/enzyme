be1f272d01aa0f34b52ae9809f8b3fc74127c0d5
Jordan Harband <ljharb@gmail.com>
2018-08-15 23:06:43 -0700

    [Tests] add `withConsoleThrows` `mocha-wrap` plugin

--- packages/enzyme-test-suite/test/_helpers/setupAdapters.js
+++ packages/enzyme-test-suite/test/_helpers/setupAdapters.js
@@ -1,4 +1,21 @@
 const Enzyme = require('enzyme');
+const wrap = require('mocha-wrap');
+
 const Adapter = require('./adapter');
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const origWarn = console.warn;
+const origError = console.error;
+wrap.register(function withConsoleThrows() {
+  return this.withOverrides(() => console, () => ({
+    error(msg) {
+      origError.apply(console, arguments); // eslint-disable-line prefer-rest-params
+      throw new EvalError(msg);
+    },
+    warn(msg) {
+      origWarn.apply(console, arguments); // eslint-disable-line prefer-rest-params
+      throw new EvalError(msg);
+    },
+  }));
+});
