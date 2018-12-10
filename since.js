bbbd6ea4f8472bb227112c98011d008ad81c6c1b
Jordan Harband <ljharb@gmail.com>
2018-12-10 01:16:13 -0800

    [meta] improve `npm run since` to include docs

--- since.js
+++ since.js
@@ -6,17 +6,20 @@ const { spawnSync } = require('child_process');
 const glob = require('glob-gitignore');
 
 const packagesDir = path.join(__dirname, 'packages');
+const docsDir = path.join(__dirname, 'docs');
 
 const packages = (process.argv.length > 2 ? [process.argv[2]] : glob.sync('*', { cwd: packagesDir }))
   .map(name => JSON.parse(fs.readFileSync(path.join(packagesDir, name, 'package.json'))))
   .filter(x => !x.private && x.name !== 'enzyme-example-mocha');
 
 packages.forEach((pkg) => {
-  const tag = `${pkg.name}@${pkg.version}`;
-  const log = spawnSync('git', ['log', '--oneline', `${tag}..HEAD`, path.join(packagesDir, pkg.name)], { stdio: 'pipe' });
+  const tag = `${pkg.name === 'docs' ? 'enzyme' : pkg.name}@${pkg.version}`;
+  const dir = path.join(packagesDir, pkg.name);
+  const logArgs = ['log', '--oneline', `${tag}..HEAD`, dir].concat(pkg.name === 'enzyme' ? docsDir : []);
+  const log = spawnSync('git', logArgs, { stdio: 'pipe' });
   if (log.stdout.length > 0 || log.stderr.length > 0) {
     console.log(tag);
-    spawnSync('git', ['log', '--oneline', `${tag}..HEAD`, path.join(packagesDir, pkg.name)], { stdio: 'inherit' });
+    spawnSync('git', logArgs, { stdio: 'inherit' });
     console.log('\n');
   }
 });
