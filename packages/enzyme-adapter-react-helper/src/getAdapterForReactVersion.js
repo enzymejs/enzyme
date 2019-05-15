db61d49eeda80476400abbec69664b8deec78b05
Andrew Huth <andrew.huth@airbnb.com>
2019-05-15 10:22:50 -0700

    [enzyme-adapter-react-helper] [fix] properly detect and handle pre-release versions

--- packages/enzyme-adapter-react-helper/src/getAdapterForReactVersion.js
+++ packages/enzyme-adapter-react-helper/src/getAdapterForReactVersion.js
@@ -1,30 +1,36 @@
 import semver from 'semver';
 
-export default function getAdapterForReactVersion(reactVersion) {
-  const normalizedVersion = semver.coerce(reactVersion);
+function removePreRelease(version) {
+  return semver.inc(version, 'patch');
+}
 
-  if (semver.satisfies(normalizedVersion, '^16.4.0-0')) {
+export default function getAdapterForReactVersion(reactVersion) {
+  const versionRange = semver.prerelease(reactVersion)
+    ? removePreRelease(reactVersion)
+    : semver.validRange(reactVersion);
+
+  if (semver.intersects(versionRange, '^16.4.0')) {
     return 'enzyme-adapter-react-16';
   }
-  if (semver.satisfies(normalizedVersion, '~16.3.0-0')) {
+  if (semver.intersects(versionRange, '~16.3.0')) {
     return 'enzyme-adapter-react-16.3';
   }
-  if (semver.satisfies(normalizedVersion, '~16.2')) {
+  if (semver.intersects(versionRange, '~16.2')) {
     return 'enzyme-adapter-react-16.2';
   }
-  if (semver.satisfies(normalizedVersion, '~16.0.0-0 || ~16.1')) {
+  if (semver.intersects(versionRange, '~16.0.0 || ~16.1')) {
     return 'enzyme-adapter-react-16.1';
   }
-  if (semver.satisfies(normalizedVersion, '^15.5.0')) {
+  if (semver.intersects(versionRange, '^15.5.0')) {
     return 'enzyme-adapter-react-15';
   }
-  if (semver.satisfies(normalizedVersion, '15.0.0 - 15.4.x')) {
+  if (semver.intersects(versionRange, '15.0.0 - 15.4.x')) {
     return 'enzyme-adapter-react-15.4';
   }
-  if (semver.satisfies(normalizedVersion, '^0.14.0')) {
+  if (semver.intersects(versionRange, '^0.14.0')) {
     return 'enzyme-adapter-react-14';
   }
-  if (semver.satisfies(normalizedVersion, '^0.13.0')) {
+  if (semver.intersects(versionRange, '^0.13.0')) {
     return 'enzyme-adapter-react-13';
   }
 
