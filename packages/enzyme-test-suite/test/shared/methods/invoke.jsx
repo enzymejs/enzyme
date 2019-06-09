05d885c1eb5797a971646f46e0ba66df3048b41c
陳乙山 <pipio1994@gmail.com>
2019-06-09 10:39:58 +0800

    [tests] `invoke`: Add failed `mount` test without explicit `act` call

--- packages/enzyme-test-suite/test/shared/methods/invoke.jsx
+++ packages/enzyme-test-suite/test/shared/methods/invoke.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon-sandbox';
+import {
+  itIf,
+} from '../../_helpers';
+import {
+  is,
+} from '../../_helpers/version';
+
+import {
+  useEffect,
+  useState,
+} from '../../_helpers/react-compat';
 
 export default function describeInvoke({
   Wrap,
   WrapperName,
+  isShallow,
 }) {
   describe('.invoke(propName)(..args)', () => {
     class CounterButton extends React.Component {
@@ -83,6 +95,33 @@ export default function describeInvoke({
       const [[arg1, arg2]] = spy.args;
       expect(arg1).to.equal(a);
       expect(arg2).to.equal(b);
+    });
+
+    // TODO: enable when the shallow renderer fixes its bug
+    itIf(!isShallow && is('>= 16.8'), 'works without explicit `act` wrapper', () => {
+      function App() {
+        const [counter, setCounter] = useState(0);
+        const [result, setResult] = useState(0);
+        useEffect(
+          () => setResult(counter * 2),
+          [counter],
+        );
+        return (
+          <button type="button" onClick={() => setCounter(input => input + 1)}>{result}</button>
+        );
+      }
+      const wrapper = Wrap(<App />);
+
+      const expected = ['0', '2', '4', '6'];
+
+      const actual = [wrapper.find('button').text()]
+        .concat(Array.from({ length: 3 }, () => {
+          wrapper.find('button').invoke('onClick')();
+          wrapper.update();
+
+          return wrapper.find('button').text();
+        }));
+      expect(actual).to.eql(expected);
     });
   });
 }
