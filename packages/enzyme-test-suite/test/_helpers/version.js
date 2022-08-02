69f2846c70014eb2e8e79749efb552ced79a5a34
eps1lon <silbermann.sebastian@gmail.com>
2022-08-02 21:29:40 +0200

    Remove unused TODO_17

--- packages/enzyme-test-suite/test/_helpers/version.js
+++ packages/enzyme-test-suite/test/_helpers/version.js
@@ -16,7 +16,3 @@ export const REACT17 = is('17');
 // The shallow renderer in react 16 does not yet support batched updates. When it does,
 // we should be able to go un-skip all of the tests that are skipped with this flag.
 export const BATCHING = !REACT16 && !REACT17;
-
-export const TODO_17 = function (condition) {
-  return REACT17 && condition;
-};
