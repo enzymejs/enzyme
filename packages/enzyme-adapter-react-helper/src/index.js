0c664aa97d2883705ec7382ae8657fc6bf215518
mrdulin <novaline.dulin@gmail.com>
2023-08-03 16:17:48 +0800

    refactor: flat the try/catch block

--- packages/enzyme-adapter-react-helper/src/index.js
+++ packages/enzyme-adapter-react-helper/src/index.js
@@ -1,52 +1,31 @@
 import { configure } from 'enzyme';
 
+const adapterNames = [
+  'enzyme-adapter-react-16',
+  'enzyme-adapter-react-16.3',
+  'enzyme-adapter-react-16.2',
+  'enzyme-adapter-react-16.1',
+  'enzyme-adapter-react-15',
+  'enzyme-adapter-react-15.4',
+  'enzyme-adapter-react-14',
+  'enzyme-adapter-react-13',
+];
 export default function setupEnzymeAdapter(enzymeOptions = {}, adapterOptions = {}) {
-  let Adapter;
-
-  try {
-    // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-    Adapter = require('enzyme-adapter-react-16');
-  } catch (R) {
+  for (let i = 0; i < adapterNames.length; i += 1) {
     try {
-      // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-      Adapter = require('enzyme-adapter-react-16.3');
-    } catch (E) {
-      try {
-        // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-        Adapter = require('enzyme-adapter-react-16.2');
-      } catch (A) {
-        try {
-          // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-          Adapter = require('enzyme-adapter-react-16.1');
-        } catch (r) {
-          try {
-            // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-            Adapter = require('enzyme-adapter-react-15');
-          } catch (e) {
-            try {
-              // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-              Adapter = require('enzyme-adapter-react-15.4');
-            } catch (a) {
-              try {
-                // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-                Adapter = require('enzyme-adapter-react-14');
-              } catch (c) {
-                try {
-                  // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved
-                  Adapter = require('enzyme-adapter-react-13');
-                } catch (t) {
-                  throw new Error('It seems as though you don’t have any `enzyme-adapter-react-*` installed. Please install the relevant version and try again.');
-                }
-              }
-            }
-          }
-        }
-      }
+      const adapterName = adapterNames[i];
+      // eslint-disable-next-line import/no-extraneous-dependencies, global-require, import/no-unresolved, import/no-dynamic-require
+      const Adapter = require(adapterName);
+      configure({
+        adapter: new Adapter(adapterOptions),
+        ...enzymeOptions,
+      });
+      return;
+    } catch (_ignoreError) {
+      // continue regardless of error
     }
   }
-
-  configure({
-    adapter: new Adapter(adapterOptions),
-    ...enzymeOptions,
-  });
+  throw new Error(
+    'It seems as though you don’t have any `enzyme-adapter-react-*` installed. Please install the relevant version and try again.',
+  );
 }
